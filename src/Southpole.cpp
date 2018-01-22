7ce12a932d49493666423f251d1dfd33e650dc6d
Gerhard Brandt <gbrandt@cern.ch>
2018-01-22 13:21:42 +0100

    correct init meta data

--- src/Southpole.cpp
+++ src/Southpole.cpp
@@ -13,8 +13,8 @@ void init(rack::Plugin *p) {
 	// The ZIP package must only contain one folder, with the name equal to the plugin's slug.
 	p->slug = "Southpole";
 	p->version = TOSTRING(VERSION);
-	p->website = "https://github.com/VCVRack/Tutorial";
-	p->manual = "https://github.com/VCVRack/Tutorial/blob/master/README.md";
+	p->website = "https://github.com/gbrandt1/southpole-vcvrack";
+	p->manual = "https://github.com/gbrandt1/southpole-vcvrack/blob/master/README.md";
 
 	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
 	//p->addModel(createModel<MyModuleWidget>("Southpole", "MyModule", "My Module", //OSCILLATOR_TAG));
