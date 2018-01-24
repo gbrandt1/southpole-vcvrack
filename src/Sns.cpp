9a4cb2a0d2cb79289389cab517d0bd8778f19a91
Gerhard Brandt <gbrandt@cern.ch>
2018-01-24 23:47:19 +0100

    progress on SNS

--- src/Sns.cpp
+++ src/Sns.cpp
@@ -4,25 +4,24 @@
 
 #include "Bjorklund.hpp"
 
-#define BUFFER_SIZE 512
-
 struct Sns : Module {
 	enum ParamIds {
 		K_PARAM,
-		N_PARAM,
 		L_PARAM,
 		S_PARAM,
+		ACCENT_PARAM,
 		CLK_PARAM,
 		TRIG_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		CLK_INPUT,
-		TRIG_INPUT,
+		RESET_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
 		GATE_OUTPUT,
+		ACCENT_OUTPUT,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
@@ -47,101 +46,112 @@ struct Sns : Module {
 };
 
 	Bjorklund euclid;
+	Bjorklund accents;
+	bool calculate;
+	bool from_reset;
 
-	int par_k; // fill
-	int par_n; // burst length
-	int par_l; // pattern length		
-	int par_s; // shift
+	int par_k = 1; // fill
+	int par_l = 1; // pattern length		
+	int par_s = 1; // shift
+	int par_a = 1; // accent
+	int par_last;
 
-	int last;
-
-	bool running = true;
-  	SchmittTrigger clockTrigger;  // for external clock
+  	SchmittTrigger clockTrigger;  // external clock
   	SchmittTrigger resetTrigger;  // reset button
+	PulseGenerator gatePulse;
+	PulseGenerator accentPulse;
 
- 	double ltime;
-  	double dTime;
-  	int bpm;
-  	double timerLength;
-	double timerTime;
+	int currentStep = 0;
+	int currentIndex;
+	int accentIndex;
 
 void Sns::reset() {
-	ltime = 0.0;
-	dTime = 1.0 / static_cast<double>(engineGetSampleRate());
-	bpm = 120;
-	timerLength = 1.0 / (static_cast<double>(bpm) / 60.0);
-	timerTime = timerLength;
+
+	euclid.reset();	
+	euclid.init(par_l,par_k);
+	euclid.iter();
+   	euclid.print();
+   	calculate = false;
+    
+	if (par_a>0) {
+		accents.reset();	
+		accents.init(par_k,par_a);
+		accents.iter();
+		accents.print();
+	}
+	from_reset = true;
 }
 
 void Sns::onSampleRateChange() {
-  dTime = 1.0 / static_cast<double>(engineGetSampleRate());
 }
 
 void Sns::step() {
 
+	// no processing during recalculation
+	if (calculate) return;
+
   	bool nextStep = false;
 
-  	// clocking
-  	if (running) {
-		ltime += dTime;
-		if (inputs[CLK_INPUT].active) {
-			if (clockTrigger.process(inputs[CLK_INPUT].value)) {
-				nextStep = true;
-			}
-		}  else {
-			timerLength = 1.0 / (static_cast<double>(bpm) / 60.0);
-			timerTime -= dTime;
-			if (dTime > timerTime) {
-				timerTime = 0.0;
-			}
-			if (timerTime <= 0.0) {
-				nextStep = true;
-				timerTime = timerLength;
-			}
+	if (inputs[CLK_INPUT].active)
+	{
+		if (clockTrigger.process(inputs[CLK_INPUT].value)) {
+			nextStep = true;
 		}
-    }
+	}  
 
-	// act on clk pulse
-	//if ( params[CLK_PARAM].value ) {
-	//}
+	// reset sequence
+	if (from_reset) { 
+		currentStep = par_l;
+		//nextStep = true;
+		from_reset = false;
+	}
+	if (inputs[RESET_INPUT].active) {
+		if (resetTrigger.process(inputs[RESET_INPUT].value)) {
+			currentStep = par_l;
+			//nextStep = true;
+		}		
+	}	
 
-	// Reset the Schmitt trigger so we don't trigger immediately if the input is high
-	//clkTrigger.reset();
+	if (nextStep) {
+		
+		currentStep++;
+		if (currentStep >= par_l) {
+		   currentStep = 0;
+		   accentIndex = 0;
+		}
+		
+		currentIndex = ( currentStep + par_s ) % par_l;
+	    if (euclid.sequence.at( currentIndex )) {
+		  	gatePulse.trigger(1e-3);
+		  	if (par_a) {
+		      if (accents.sequence.at( accentIndex )) {
+			    accentPulse.trigger(1e-3);
+			  }					
+			  accentIndex ++;
+			} 
+		} 
+	} 
 
-	// Must go below 0.1V to trigger
-	//resetTrigger.setThresholds(params[TRIG_PARAM].value - 0.1, params[TRIG_PARAM].value);
-	//float gate = external ? inputs[TRIG_INPUT].value : inputs[X_INPUT].value;
 
-	// Reset if triggered
-	//float holdTime = 0.1;
-	//if (resetTrigger.process(gate) || (frameIndex >= engineGetSampleRate() * holdTime)) {
-	//		bufferIndex = 0; frameIndex = 0; return;
-	//}
+	bool gpulse = gatePulse.process(1.0 / engineGetSampleRate());
+	bool apulse = accentPulse.process(1.0 / engineGetSampleRate());
 
-	// Reset if we've waited too long
-	//	if (frameIndex >= engineGetSampleRate() * holdTime) {
-	//bufferIndex = 0; frameIndex = 0; return;
-	//}
-	//}
-//}
-//void Sns::onChange(EventChange &e) override {
+	outputs[GATE_OUTPUT].value = gpulse ? 10.0 : 0.0;
+	outputs[ACCENT_OUTPUT].value = apulse ? 10.0 : 0.0;
 
 	par_l = int(1.+15.*params[Sns::L_PARAM].value);		
-	par_s = int((par_l-1.)*params[Sns::S_PARAM].value);
-	par_n = int(1.+(par_l-1.)*params[Sns::N_PARAM].value);
-	par_k = int(1.+(par_n-1.)*params[Sns::K_PARAM].value);
+	par_s = par_l-int((par_l-1.)*params[Sns::S_PARAM].value);
+	par_k = int(1.+(par_l-1.)*params[Sns::K_PARAM].value);
+	par_a = float(par_k)*params[Sns::ACCENT_PARAM].value;
 	
 	// new sequence in case of change to parameters
-	if (par_l+par_s+par_n+par_k != last) {
-       printf("Pattern for %d %d %d %d\n",par_k,par_n,par_l,par_s);
-	   last = par_l+par_s+par_n+par_k;
-
-	   euclid.reset();	
-	   euclid.init(par_n,par_k);
-	   euclid.iter();
-	   euclid.print();
-	}
+	if (par_l+par_s+par_a+par_k != par_last) {
 	
+//		printf("%d %d",par_k,par_a);	
+	   par_last = par_l+par_s+par_a+par_k;
+	   calculate = true;
+	   reset();		// to do: don't call in step ...
+	}	
 }
 
 
@@ -155,9 +165,8 @@ struct SnsDisplay : TransparentWidget {
 	  font = Font::load(assetPlugin(plugin, "res/hdad-segment14-1.002/Segment14.ttf"));
 	}
 
-	void drawPolygon(NVGcontext *vg, float k, float n, float l, float s) {
-
-
+	void drawPolygon(NVGcontext *vg) {
+		
 		//nvgSave(vg);
 		Rect b = Rect(Vec(2, 2), box.size.minus(Vec(2, 2)));
 		//nvgScissor(vg, b.pos.x, b.pos.y, b.size.x, b.size.y);
@@ -166,56 +175,78 @@ struct SnsDisplay : TransparentWidget {
 		float cy = 0.5*b.size.y;
 		float  r = 40.;
 
+		// empty circles for l total steps
 		nvgStrokeColor(vg, nvgRGBA(0xff, 0x00, 0x00, 0x7f));
 		nvgBeginPath(vg);
 	    nvgCircle(vg, cx, cy, r);
-		for (int i = 0; i < l; i++) {
-			float x = cx + r * cosf(2.*M_PI*i/l-.5*M_PI);
-			float y = cy + r * sinf(2.*M_PI*i/l-.5*M_PI);
+	    nvgCircle(vg, cx, cy, 0.5*r);
+		for (int i = 0; i < par_l; i++) {
+			float x = cx + r * cosf(2.*M_PI*i/par_l-.5*M_PI);
+			float y = cy + r * sinf(2.*M_PI*i/par_l-.5*M_PI);
     	    nvgCircle(vg, x, y, 3.);
 		}
 		nvgStroke(vg);
 
+		// circle for current step, filled if active step
+
+		int acc = 0;
+		float x = cx + r * cosf(2.*M_PI*currentStep/par_l-.5*M_PI);
+		float y = cy + r * sinf(2.*M_PI*currentStep/par_l-.5*M_PI);
+		nvgStrokeWidth(vg, 1.5);
+		nvgFillColor(vg, nvgRGBA(0xff, 0x00, 0x00, 0xff));
+		nvgBeginPath(vg);
+		nvgCircle(vg, x, y, 3.);				
+		if ( euclid.sequence.at( currentIndex )) nvgFill(vg);
+		nvgStroke(vg);
+
+
 		nvgStrokeColor(vg, nvgRGBA(0xff, 0x00, 0x00, 0xff));
 		nvgBeginPath(vg);
-		for (int i = 0; i < n; i++) {
-			float a = (i+s)/l;
-			float x = cx + r * cosf(2.*M_PI*a-.5*M_PI);
-			float y = cy + r * sinf(2.*M_PI*a-.5*M_PI);
-			Vec p(x,y);
-			if (i==0) nvgMoveTo(vg, p.x, p.y);
-			else 
-			if ( euclid.sequence.at(i) ) {
-				nvgLineTo(vg, p.x, p.y);
+		bool first = true;
+		int acc = 0;
+		for (int i = 0; i < par_l; i++) {
+			float a = i/float(par_l);
+			float x = cx + .5*r * cosf(2.*M_PI*a-.5*M_PI);
+			float y = cy + .5*r * sinf(2.*M_PI*a-.5*M_PI);
+			if ( euclid.sequence.at( (i+par_s) % par_l )) {
+				if (par_a) {
+					if (accents.sequence.at( acc++ )) {
+						x = cx + r * cosf(2.*M_PI*a-.5*M_PI);
+						y = cy + r * sinf(2.*M_PI*a-.5*M_PI);
+					}	
+				}	
+				Vec p(x,y);
+				if (par_k == 1) nvgCircle(vg, x, y, 3.);				
+				if (first) {
+					nvgMoveTo(vg, p.x, p.y);
+				 	first = false;
+				} else 
+					nvgLineTo(vg, p.x, p.y);
 			}
 		}
 		nvgClosePath(vg);
-
-		//nvgLineCap(vg, NVG_ROUND);
-		//nvgMiterLimit(vg, 2.0);
 		nvgStrokeWidth(vg, 1.5);
-		//nvgGlobalCompositeOperation(vg, NVG_LIGHTER);
 		nvgStroke(vg);
-		//nvgResetScissor(vg);
-		//nvgRestore(vg);
-		
 	}
 
 	void draw(NVGcontext *vg) override {
 
-		drawPolygon(vg, par_k, par_n, par_l, par_s);
+		// wait until in defined state
+		if (calculate) return;
+
+		drawPolygon(vg); //, par_k, par_n, par_l, par_s);
 
 		nvgFontSize(vg, 11);
 		nvgFontFaceId(vg, font->handle);
 		nvgTextLetterSpacing(vg, 2.);
 
-		Vec textPos = Vec(8, 8);
+		Vec textPos = Vec(8, 146);
 		NVGcolor textColor = nvgRGB(0xff, 0x00, 0x00);
 		nvgFillColor(vg, nvgTransRGBA(textColor, 16));
-		nvgText(vg, textPos.x, textPos.y, "~~~~", NULL);
+		//nvgText(vg, textPos.x, textPos.y, "~~~~", NULL);
 		nvgFillColor(vg, textColor);
 		char str[20];
-		snprintf(str,sizeof(str),"%d %d %d %d",int(par_k),int(par_n),int(par_l),int(par_s));
+		snprintf(str,sizeof(str),"%d %d %d %d",int(par_k),int(par_l),int(par_l-par_s),int(par_a));
 		nvgText(vg, textPos.x, textPos.y, str, NULL);
 
 	}
@@ -242,13 +273,21 @@ SnsWidget::SnsWidget() {
 		addChild(display);
 	}
 
-	addParam(createParam<sp_SmallBlackKnob>(Vec(15   , 209), module, Sns::K_PARAM, 0., 1., .5));
-	addParam(createParam<sp_SmallBlackKnob>(Vec(15+25, 209), module, Sns::N_PARAM, 0., 1., .5));
-	addParam(createParam<sp_SmallBlackKnob>(Vec(15+50, 209), module, Sns::L_PARAM, 0., 1., 1.));
-	addParam(createParam<sp_SmallBlackKnob>(Vec(15+70, 209), module, Sns::S_PARAM, 0., 1., 0.));
+	float yh = 30;
 
-//	addInput(createInput<PJ301MPort>(Vec(17, 319), module, Sns::X_INPUT));
+	addParam(createParam<sp_SmallBlackKnob>(Vec(4   , 209), module, Sns::K_PARAM, 0., 1., .25));
+	addParam(createParam<sp_SmallBlackKnob>(Vec(4+25, 209), module, Sns::L_PARAM, 0., 1., 1.));
+	addParam(createParam<sp_SmallBlackKnob>(Vec(4+50, 209), module, Sns::S_PARAM, 0., 1., 0.));
+	addParam(createParam<sp_SmallBlackKnob>(Vec(4   , 209+yh), module, Sns::ACCENT_PARAM, 0., 1., 0.));
 
-	addChild(createLight<SmallLight<GreenLight>>(Vec(104, 251), module, Sns::CLK_LIGHT));
+	addInput(createInput<sp_Port>(Vec(17, 319-30), module, Sns::CLK_INPUT));
+	addInput(createInput<sp_Port>(Vec(17+25, 319-30), module, Sns::RESET_INPUT));
+
+	addOutput(createOutput<sp_Port>(Vec(17+25, 319), module, Sns::GATE_OUTPUT));
+	addOutput(createOutput<sp_Port>(Vec(17+50, 319), module, Sns::ACCENT_OUTPUT));
+
+	//addChild(createLight<SmallLight<RedLight>>(Vec(4, 281), module, Sns::CLK_LIGHT));
+	//addChild(createLight<SmallLight<RedLight>>(Vec(4+25, 281), module, Sns::GATE_LIGHT));
+	//addChild(createLight<SmallLight<RedLight>>(Vec(4+50, 281), module, Sns::ACCENT_LIGHT));
 
 }
