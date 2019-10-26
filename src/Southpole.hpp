374399c57b1a3a9dbfbf082aa6fe52107f9c4426
Netboy3 <1472804+netboy3@users.noreply.github.com>
2019-10-25 20:36:06 -0400

    Remove unnecessary components

--- src/Southpole.hpp
+++ src/Southpole.hpp
@@ -15,32 +15,11 @@ struct sp_Port : app::SvgPort {
   }
 };
 
-struct sp_Switch : app::SvgSwitch {
-  sp_Switch() {
-    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-switchv_0.svg")));
-    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-switchv_1.svg")));
-  }
-};
-
-struct sp_Encoder : app::SvgKnob {
-  sp_Encoder() {
-    minAngle = -1.0f * M_PI;
-    maxAngle = 1.0f * M_PI;
-    setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-encoder.svg")));
-    //sw->svg = APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-encoder.svg"));
-    //sw->wrap();
-    //box.size = sw->box.size;
-  }
-};
-
 struct sp_BlackKnob : app::SvgKnob {
   sp_BlackKnob() {
     minAngle = -0.83 * M_PI;
     maxAngle = 0.83 * M_PI;
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-knobBlack.svg")));
-    //sw->svg = APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-knobBlack.svg"));
-    //sw->wrap();
-    //box.size = Vec(32,32);
   }
 };
 
@@ -49,9 +28,6 @@ struct sp_SmallBlackKnob : app::SvgKnob {
     minAngle = -0.83 * M_PI;
     maxAngle = 0.83 * M_PI;
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-knobBlack.svg")));
-    //sw->svg = APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-knobBlack.svg"));
-    //sw->wrap();
-    //box.size = Vec(20,20);
   }
 };
 
@@ -60,8 +36,5 @@ struct sp_Trimpot : app::SvgKnob {
     minAngle = -0.83 * M_PI;
     maxAngle = 0.83 * M_PI;
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-trimpotBlack.svg")));
-    //sw->svg = APP->window->loadSvg(asset::plugin(pluginInstance, "res/sp-knobBlack.svg"));
-    //sw->wrap();
-    //box.size = Vec(18,18);
   }
 };
